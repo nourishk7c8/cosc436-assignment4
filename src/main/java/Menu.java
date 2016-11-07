374e17836a1c0a74637c98b7934e31aaf629d60a
Nourish L. Khan <nourish.khan@gmail.com>
2016-11-07 18:57:07 -0500

    HEART_HEALTHY and NOT_HEART_HEALTHY changed to constants (not sure why they weren't already)

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Menu {
-    public static boolean HEART_HEALTHY = true;
-    public static boolean NOT_HEART_HEALTHY = false;
+    public static final boolean HEART_HEALTHY = true;
+    public static final boolean NOT_HEART_HEALTHY = false;
 
     private List<MenuItem> menu;
     private MenuItem firstItem;    //constructor sets to null; updated after first append
